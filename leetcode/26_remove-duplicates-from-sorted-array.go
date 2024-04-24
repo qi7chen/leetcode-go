b44360c77b439f3ae6b55b006ae30b4f6098b1b0
qchen <ichenq@outlook.com>
2024-04-24 16:01:37 +0800

    remove duplicates from sorted array

--- leetcode/26_remove-duplicates-from-sorted-array.go
+++ leetcode/26_remove-duplicates-from-sorted-array.go
@@ -1,1 +1,23 @@
 package leetcode
+
+// https://leetcode.cn/problems/remove-duplicates-from-sorted-array
+//
+// 用2个指针，一个在前记作 p，一个在后记作 q，算法流程如下：
+// 比较 p 和 q 位置的元素是否相等。
+// 如果相等，q 后移 1 位
+// 如果不相等，将 q 位置的元素复制到 p+1 位置上，p 后移一位，q 后移 1 位
+// 重复上述过程，直到 q 等于数组长度。
+func removeDuplicates(nums []int) int {
+	if len(nums) < 2 {
+		return len(nums)
+	}
+	var p, q = 0, 1
+	for q < len(nums) {
+		if nums[p] != nums[q] {
+			p++
+			nums[p] = nums[q]
+		}
+		q++
+	}
+	return p + 1
+}
