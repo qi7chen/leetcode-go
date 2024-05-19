9cb51313fc3e1943b4a031410f5e13b36cd84f80
qchen <ichenq@outlook.com>
2024-05-19 23:02:22 +0800

    add

---
+++ leetcode/429_n-ary-tree-level-order-traversal.go
@@ -0,0 +1,28 @@
+package leetcode
+
+// https://leetcode.cn/problems/n-ary-tree-level-order-traversal/
+// N 叉树的层序遍历
+
+func nAryLevelOrder(root *Node) [][]int {
+	if root == nil {
+		return nil
+	}
+	var result [][]int
+	var queue = []*Node{root}
+	for len(queue) > 0 {
+		var size = len(queue)
+		var level = make([]int, 0, size)
+		for i := 0; i < size; i++ {
+			var node = queue[i]
+			for _, child := range node.Children {
+				if child != nil {
+					queue = append(queue, child)
+				}
+			}
+			level = append(level, node.Val)
+		}
+		queue = queue[size:]
+		result = append(result, level)
+	}
+	return result
+}
---
+++ leetcode/429_n-ary-tree-level-order-traversal_test.go
@@ -0,0 +1,25 @@
+package leetcode
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_nAryLevelOrder(t *testing.T) {
+	type args struct {
+		root *Node
+	}
+	tests := []struct {
+		name string
+		args args
+		want [][]int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			assert.Equalf(t, tt.want, nAryLevelOrder(tt.args.root), "nAryLevelOrder(%v)", tt.args.root)
+		})
+	}
+}
